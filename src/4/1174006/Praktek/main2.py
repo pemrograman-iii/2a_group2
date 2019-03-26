8704472db8f4ae23e092e4668446563be3104eb4
Diva Krishna <divakrishnam@gmail.com>
2019-03-26 16:26:50 +0700

    Chapter 4 Diva

---
+++ src/4/1174006/Praktek/main2.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 25 21:40:25 2019
+
+@author: vanerz
+"""
+
+lib = __import__('1174006pandas')
+
+lib.bukaModeListPandas()
+lib.bukaModeDictPandas()
+
+lib.tulisCsvPandas()
