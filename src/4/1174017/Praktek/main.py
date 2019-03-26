991bf735a54153b9b41e30eb515c78537208a0d2
MuhRifkyPrananda <ikkyrifky08@gmail.com>
2019-03-27 00:00:12 +0700

    rifky

---
+++ src/4/1174017/Praktek/main.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 26 20:55:57 2019
+
+@author: ASUS
+"""
+
+lib = __import__('17_csv')
+
+lib.bukaModeListCsv()
+lib.bukaModeDictCsv()
+
+lib.tulisCsv()
---
+++ src/4/1174017/Praktek/main2.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 26 20:56:39 2019
+
+@author: ASUS
+"""
+
+lib = __import__('17_pandas')
+
+lib.bukaModeListPandas()
+lib.bukaModeDictPandas()
+
+lib.tulisCsvPandas()
