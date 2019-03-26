a009576f8bce1f133ef4f8c910eec254b0d27b4e
MhmadTomy <tomymaulidu@gmail.com>
2019-03-26 23:08:31 +0700

    Tomy

---
+++ src/4/1174031/praktek/main.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 25 21:40:17 2019
+
+@author: NITRO 5
+"""
+
+lib = __import__('1174031csv')
+
+lib.bukaModeListCsv()
+lib.bukaModeDictCsv()
+
+lib.tulisCsv()
